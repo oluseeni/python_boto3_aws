94024d0a5cf6a9bff94e7d5a87adf0b171ad3cec
seeni idowu <seeni.idowu@gmail.com>
2022-11-10 19:16:52 +0000

    create s3 bucket

---
+++ create_s3_bucket.py
@@ -0,0 +1,14 @@
+import boto3
+
+aws_resource = boto3.resource("s3")
+bucket = aws_resource.Bucket("bucket0330")
+
+response = bucket.create(
+    ACL = 'private',
+    CreateBucketConfiguration = {
+        'LocationConstraint':'us-east-2'
+    },
+)
+
+print(response)
+
